0ec8bc43d3b5cd5f4b7e795e9840d69c64d9d934
nabilgharbi <gharbi.nabill@gmail.com>
2023-12-12 13:16:05 +0100

    first commit: Added All Files

---
+++ Likes.js
@@ -0,0 +1,23 @@
+var likeCount1 = 9;
+var likeSpan1 = document.querySelector("#post-1");
+
+function like1() {
+    likeCount1++;
+    likeSpan1.innerText = likeCount1 + " like(s)";
+}
+
+var likeCount2 = 12;
+var likeSpan2 = document.querySelector("#post-2");
+
+function like2() {
+    likeCount2++;
+    likeSpan2.innerText = likeCount2 + " like(s)";
+}
+
+var likeCount3 = 9;
+var likeSpan3 = document.querySelector("#post-3");
+
+function like3() {
+    likeCount3++;
+    likeSpan3.innerText = likeCount3 + " like(s)";
+}
